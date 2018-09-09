7ae241da3851d926ed96ee149b009a9e6ff0ae05
utlime <utlime@gmail.com>
2018-09-09 12:55:38 +0700

    feat:

--- src/tag/tagDefaults.ts
+++ src/tag/tagDefaults.ts
@@ -1,16 +1,18 @@
-import { clone, defaults } from 'lodash';
-import ITag from './ITag';
+import { clone, defaults } from 'lodash'
+import ITag from './ITag'
 
 interface IFunctionOptions {
-  name: string;
-  color?: string;
+  name: string
+  color?: string
 }
 
-const defaultTodo = {
-  color: '#FFF',
-};
+const defaultTag = {
+  color: '#FFF'
+}
 
-export default <O extends IFunctionOptions>(options?: O) =>
-  <T extends object, U extends T & O & ITag>(tag?: T): U => (
-    defaults(clone(tag), options, defaultTodo) as U
-  );
+export default <O extends IFunctionOptions>(options?: O) => <
+  T extends object,
+  U extends T & O & ITag
+>(
+  tag?: T
+): U => defaults(clone(tag), options, defaultTag) as U
