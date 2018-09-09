dba15799c1d2b1277915d850adcd1ed61ec9d067
utlime <utlime@gmail.com>
2018-09-09 18:32:47 +0700

    build(avoid aliases):

---
+++ src/tag/index.ts
@@ -0,0 +1,6 @@
+export { default as ITagCollection } from './ITagCollection'
+export { default as tagCollectionAdd } from './tagCollectionAdd'
+export { default as tagCollectionHas } from './tagCollectionHas'
+export { default as tagCollectionRemove } from './tagCollectionRemove'
+export { default as tagDefaults } from './tagDefaults'
+export { default as tagUpdate } from './tagUpdate'
---
+++ src/todo/index.ts
@@ -0,0 +1,4 @@
+export { default as ITodo } from './ITodo'
+export { default as todoDefaults } from './todoDefaults'
+export { default as todoIsDone } from './todoIsDone'
+export { default as todoUpdate } from './todoUpdate'
