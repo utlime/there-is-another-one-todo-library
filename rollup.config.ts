888f4f5358e2a16367aabe8a40b0867149dab689
utlime <utlime@gmail.com>
2018-09-15 14:30:55 +0700

    build(rollup): fixes

--- rollup.config.ts
+++ rollup.config.ts
@@ -9,34 +9,25 @@ import typescript from 'rollup-plugin-typescript2'
 import json from 'rollup-plugin-json';
 
 const pkg = require('./package.json');
-const libraryName = 'there-is-another-one-todo-library';
 
 export default {
   input: `src/index.ts`,
   output: [
-    { file: pkg.main, format: 'umd', sourcemap: true, name: camelCase(libraryName) },
+    { file: pkg.main, format: 'umd', sourcemap: true, name: camelCase(pkg.name) },
     { file: pkg.module, format: 'es', sourcemap: true },
   ],
   watch: {
     include: 'src/**',
   },
   plugins: [
-    // Allow json resolution
     json(),
-    // Compile TypeScript files
     typescript({ useTsconfigDeclarationDir: true }),
-    // Allow bundling cjs modules (unlike webpack, rollup doesn't understand cjs)
     commonjs(),
-    // Allow node_modules resolution, so you can use 'external' to control
-    // which external modules to include in the bundle
-    // https://github.com/rollup/rollup-plugin-node-resolve#usage
     resolve({
-      // pass custom options to the resolve plugin
       customResolveOptions: {
         moduleDirectory: 'node_modules'
       }
     }),
-    // Resolve source maps to the original source
     sourceMaps(),
   ],
 }
